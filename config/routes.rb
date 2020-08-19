ac358168d7cf19c0a9328dedca42a61558297a4e
keiba804 <mottuso0017@gmail.com>
2020-08-19 14:54:24 +0000

    start bookers001

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/new'
+  get 'books/create'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
