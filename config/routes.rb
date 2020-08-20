930498679329e3069d72729b00bf41804936926e
keiba804 <mottuso0017@gmail.com>
2020-08-20 07:20:48 +0000

    edit controllers & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/new'
-  get 'books/create'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
+  root to: 'books#top'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
